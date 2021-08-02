f5bf8b7fff295e2ee3e1fefae1cd425c1e2bb5b9
shrikantbiradar3449 <shrikantbiradar.study@gmail.com>
2021-08-02 18:06:13 +0530

    Added Enum locator concept in CArtPage.java

--- src/main/java/pageObjects/CartPage.java
+++ src/main/java/pageObjects/CartPage.java
@@ -1,5 +1,6 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,8 +8,9 @@ import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
  
 public class CartPage {
-	
+	private WebDriver pageDriver;
 	public CartPage(WebDriver driver) {
+		this.pageDriver = driver;
 		PageFactory.initElements(driver, this);
 	}
 	
@@ -28,5 +30,25 @@ public class CartPage {
 		try { Thread.sleep(5000);}
 		catch (InterruptedException e) {}
 	}
-	
+
+//###############  Another way to store elements #############################
+	public void addCP(){
+		pageDriver.findElement(ELEMENT.ADD_CP.findBy).click();
+		pageDriver.findElement(ELEMENT.ADD_PARTNER.findBy).click();
+	}
+	private enum ELEMENT{
+		ADD_CP(By.xpath("//span[text()='Add Controlling Party']")),
+		ADD_PARTNER(By.id("addPartner"));
+
+		private By findBy;
+
+		ELEMENT(By locator){
+			this.findBy = locator;
+		}
+
+
+
+
+	}
+	//###############  Another way to store elements End #############################
 }
